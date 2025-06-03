756d2a714a3ddb20f717b0c2a15d1f57d03a3979
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-06-03 19:33:35 -0400

    Add project files.

---
+++ Plugin.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using System;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace iiMenu.AutoUpdater
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            Console.Title = "ii's Stupid Menu // Loading...";
+
+            using (UnityWebRequest request = UnityWebRequest.Get("https://github.com/iiDk-the-actual/iis.Stupid.Menu/releases/latest/download/iis_Stupid_Menu.dll"))
+            {
+                UnityWebRequestAsyncOperation operation = request.SendWebRequest();
+
+                while (!operation.isDone) { }
+
+                if (request.result == UnityWebRequest.Result.Success)
+                {
+                    Assembly assembly = Assembly.Load(request.downloadHandler.data);
+                    Type Plugin = assembly.GetType("iiMenu.Plugin");
+
+                    if (typeof(MonoBehaviour).IsAssignableFrom(Plugin))
+                        gameObject.AddComponent(Plugin);
+                }
+            }
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace iiMenu.AutoUpdater
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.goldentrophy.gorillatag.iimenu.autoupdater";
+        public const string Name = "iiMenu.AutoUpdater";
+        public const string Version = "1.0.0";
+    }
+}
